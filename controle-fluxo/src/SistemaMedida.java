ea0c082c3a304634686a01390876b4c3a9a008ea
Naydgew <naydgew@gmail.com>
2025-04-11 12:26:51 -0300

    Estrutura condicionais

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,18 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "G";
+
+        if (sigla == "P")
+            System.out.println("Pequeno");
+        else if (sigla == "M")
+            System.out.println("MEDIO");
+        else if (sigla == "G")
+            System.out.println("GRANDE");
+        else
+            System.out.println("INDEFINIDO");
+
+            
+        
+    }
+    
+}
